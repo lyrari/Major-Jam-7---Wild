70246b3d920e260e1c7d9132b4a9d984ea8a0c55
lyrari <lyrariley1@gmail.com>
2025-05-31 01:16:10 -0700

    Use relative path for wwise addressables

---
+++ Assets/WwiseUnityAddressables-24.1.5/Editor/BuildScriptWwisePacked.cs
@@ -0,0 +1,95 @@
+﻿/*******************************************************************************
+The content of this file includes portions of the proprietary AUDIOKINETIC Wwise
+Technology released in source code form as part of the game integration package.
+The content of this file may not be used without valid licenses to the
+AUDIOKINETIC Wwise Technology.
+Note that the use of the game engine is subject to the Unity(R) Terms of
+Service at https://unity3d.com/legal/terms-of-service
+ 
+License Usage
+ 
+Licensees holding valid licenses to the AUDIOKINETIC Wwise Technology may use
+this file in accordance with the end user license agreement provided with the
+software or, alternatively, in accordance with the terms contained
+in a written agreement between you and Audiokinetic Inc.
+Copyright (c) 2024 Audiokinetic Inc.
+*******************************************************************************/
+
+#if AK_WWISE_ADDRESSABLES && UNITY_ADDRESSABLES
+
+using System;
+using UnityEditor;
+using UnityEditor.AddressableAssets;
+using UnityEditor.AddressableAssets.Build.DataBuilders;
+using UnityEditor.AddressableAssets.Settings;
+using UnityEditor.AddressableAssets.Settings.GroupSchemas;
+using UnityEngine;
+
+namespace AK.Wwise.Unity.WwiseAddressables
+{
+	/*
+	 * The wwise build script ONLY builds the bundles automatically generated groupes (e.g. WwiseData_[platform] and  WwiseData_[platform]_InitBank) for the target build platform.
+	 */
+	[CreateAssetMenu(fileName = "BuildScriptWwisePacked.asset", menuName = "Addressables/Content Builders/Wwise Build Script")]
+	public class BuildScriptWwisePacked : BuildScriptPackedMode
+	{
+		/// <inheritdoc />
+		public override string Name
+		{
+			get
+			{
+				return "Wwise Build Script";
+			}
+		}
+		/// <inheritdoc />
+		/// 
+		protected override string ProcessGroup(AddressableAssetGroup assetGroup, AddressableAssetsBuildContext aaContext)
+		{
+			if (assetGroup == null)
+				return string.Empty;
+
+			var buildTarget = (BuildTarget) Enum.Parse(typeof(BuildTarget), aaContext.runtimeData.BuildTarget);
+			IncludePlatformSpecificBundles(buildTarget);
+
+			foreach (var schema in assetGroup.Schemas)
+			{
+				var errorString = ProcessGroupSchema(schema, assetGroup, aaContext);
+				if (!string.IsNullOrEmpty(errorString))
+					return errorString;
+			}
+
+			return string.Empty;
+		}
+
+		private void IncludePlatformSpecificBundles(UnityEditor.BuildTarget target)
+		{
+			var wwisePlatform = AkAddressablesEditorUtilities.GetWwisePlatformNameFromBuildTarget(target);
+
+			var addressableSettings = AddressableAssetSettingsDefaultObject.Settings;
+
+			if (addressableSettings == null)
+			{
+				UnityEngine.Debug.LogWarningFormat("[Addressables] settings file not found.\nPlease go to Menu/Window/Asset Management/Addressables/Groups, then click 'Create Addressables Settings' button.");
+				return;
+			}
+
+			foreach (var group in addressableSettings.groups)
+			{
+				var include = false;
+
+				if (group.Name.Contains("WwiseData"))
+				{
+					if (group.Name.Contains(wwisePlatform))
+					{
+						include = true;
+					}
+
+					var bundleSchema = group.GetSchema<BundledAssetGroupSchema>();
+					if (bundleSchema != null)
+						bundleSchema.IncludeInBuild = include;
+				}
+			}
+		}
+	}
+}
+#endif
---
+++ Assets/WwiseUnityAddressables-24.1.5/Editor/WwiseStreamingAssetImporter.cs
@@ -0,0 +1,59 @@
+﻿/*******************************************************************************
+The content of this file includes portions of the proprietary AUDIOKINETIC Wwise
+Technology released in source code form as part of the game integration package.
+The content of this file may not be used without valid licenses to the
+AUDIOKINETIC Wwise Technology.
+Note that the use of the game engine is subject to the Unity(R) Terms of
+Service at https://unity3d.com/legal/terms-of-service
+ 
+License Usage
+ 
+Licensees holding valid licenses to the AUDIOKINETIC Wwise Technology may use
+this file in accordance with the end user license agreement provided with the
+software or, alternatively, in accordance with the terms contained
+in a written agreement between you and Audiokinetic Inc.
+Copyright (c) 2025 Audiokinetic Inc.
+*******************************************************************************/
+
+#if AK_WWISE_ADDRESSABLES && UNITY_ADDRESSABLES
+
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using System.IO;
+using System.Security.Cryptography;
+
+using UnityEditor.AssetImporters;
+
+namespace AK.Wwise.Unity.WwiseAddressables
+{
+	[ScriptedImporter(1, "wem")]
+	public class WwiseStreamingAssetImporter : ScriptedImporter
+	{
+		public override void OnImportAsset(AssetImportContext ctx)
+		{
+			string assetName = Path.GetFileNameWithoutExtension(ctx.assetPath);
+
+			string platform;
+			string language;
+			string type;
+			AkAddressablesEditorUtilities.ParseAssetPath(ctx.assetPath, out platform, out language, out type);
+
+			if (platform == null)
+			{
+				return;
+			}
+
+			WwiseStreamingMediaAsset dataAsset = ScriptableObject.CreateInstance<WwiseStreamingMediaAsset>();
+			dataAsset.RawData = File.ReadAllBytes(Path.GetFullPath(ctx.assetPath));
+			byte[] hash = MD5.Create().ComputeHash(dataAsset.RawData);
+			dataAsset.hash = hash;
+			dataAsset.language = language;
+
+			ctx.AddObjectToAsset(string.Format("WwiseSteamingMedia_{0}{1}_{2}", platform, language, assetName), dataAsset);
+			ctx.SetMainObject(dataAsset);
+		}
+	}
+}
+#endif
